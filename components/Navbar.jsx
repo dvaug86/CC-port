7c2c65d61a1e45b60f370bfd4c59ea5ab5468a1a
dv <dvaug86@gmail.com>
2023-01-26 13:56:24 -0500

    smooth scrolling added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,45 +1,64 @@
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
-import {FaGithub, FaLinkedinIn} from "react-icons/fa";
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 import NavLogo from "../public/assets/navLogo.png";
 
 const Navbar = () => {
-    const [nav, setNav] =useState(false);
-    
-const handleNav = () => {
-    setNav(!nav) 
-}
+  const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+  }, []);
 
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100]">
+    <div
+      className={
+        shadow
+          ? "fixed w-full h-20 shadow-xl z-[100]"
+          : "fixed w-full h-20 z-[100]"
+      }
+    >
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image src={NavLogo} alt="/" width="125" height="50" />
+        <Link href="home" scroll={false}>
+          <Image src={NavLogo} alt="/" width="125" height="50" />
+        </Link>
         <div className="">
           {/* full screen navbar menu */}
           {/* This is hidden when screen is smaller than "medium", anything larger itll show as flex row and b.c of earlier div it is justified between  */}
           <ul className="hidden md:flex">
-            <Link href="/">
+            <Link href="/#home" scroll={false}>
               <li className=" ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
-            <Link href="/">
+            <Link href="/#about" scroll={false}>
               <li className=" ml-10 text-sm uppercase hover:border-b">About</li>
             </Link>
-            <Link href="/">
+            <Link href="/#skills" scroll={false}>
               <li className=" ml-10 text-sm uppercase hover:border-b">
                 Skills
               </li>
             </Link>
-            <Link href="/">
+            <Link href="/#projects" scroll={false}>
               <li className=" ml-10 text-sm uppercase hover:border-b">
                 Projects
               </li>
             </Link>
-            <Link href="/">
+            <Link href="/#contact" scroll={false}>
               <li className=" ml-10 text-sm uppercase hover:border-b">
                 Contact
               </li>
@@ -56,66 +75,73 @@ const handleNav = () => {
       {/* overlay */}
       <div
         className={
-          nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
         }
       >
-      {/* small menu items */}
-      {/* header for small menu */}
-      <div
+        {/* small menu items */}
+        {/* header for small menu */}
+        <div
           className={
             nav
-              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
-              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
           }
         >
           <div>
             <div className="flex  w-full items-center justify-between">
               <Image src={NavLogo} alt="/" width="87" height="35" />
-              <div onClick={handleNav}  className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              >
                 <AiOutlineClose />
               </div>
             </div>
             <div className="border-b border-gray-300 my-4">
-                <p className="w-[85%] md:w-[90%] py-4">Let's Build Something Legendary Together</p>
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let's Build Something Legendary Together
+              </p>
             </div>
           </div>
           <div className="py-4 flex flex-col">
-           {/* navbar links */}
+            {/* navbar links */}
             <ul className="uppercase">
-                <Link href="/">
+              <Link href="/#home" scroll={false}>
                 <li className="py-4 text-sm">Home</li>
-                </Link>
-                <Link href="/">
+              </Link>
+              <Link href="/#about" scroll={false}>
                 <li className="py-4 text-sm">About</li>
-                </Link>
-                <Link href="/">
+              </Link>
+              <Link href="/#skills" scroll={false}>
                 <li className="py-4 text-sm">Skills</li>
-                </Link>
-                <Link href="/">
+              </Link>
+              <Link href="/#projects" scroll={false}>
                 <li className="py-4 text-sm">Projects</li>
-                </Link>
-                <Link href="/">
+              </Link>
+              <Link href="/#contacts" scroll={false}>
                 <li className="py-4 text-sm">Contact</li>
-                </Link>
+              </Link>
             </ul>
 
             {/* side menu footer items */}
             <div className="pt-40">
-                <p className="uppercase tracking-widest text-[#5651e5]">Let's Connect</p>
-                <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
-                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
-                    <FaLinkedinIn />
-                    </div>
-                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
-                    <FaGithub />
-                    </div>
-                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
-                    <AiOutlineMail />
-                    </div>
-                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
-                    <BsFillPersonLinesFill />
-                    </div>
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let's Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaLinkedinIn />
                 </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
             </div>
           </div>
         </div>
